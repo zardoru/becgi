a4e731678ef3997fc2d2d0acbafbb9db2a6a945b
zardoru <forsakengmex@gmail.com>
2015-11-16 16:42:08 -0300

    Add some stuff to make this repo run in heroku.

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 app.config.from_object("config")
 database.generate()
 
+# app vars (to be used etc)
+submissions_open = False
+impressions_open = False
+
 @app.route('/')
 def submissions_list():
     return render_template("about.html")
@@ -33,20 +37,26 @@ class ImpressionForm(Form):
 def submit_bms(form=None):
     if form is None:
         form = SubmitForm()
-    return render_template("submit.html", form=form)
+    if submissions_open:
+        return render_template("submit.html", form=form)
+    else:
+        return render_template("section_closed.html")
 
 @app.route('/submit/handle_submit', methods=['POST'])
 def handle_bms_submission():
-    form = SubmitForm()
-    if form.validate_on_submit():
-        name = form.bms_name.data
-        author = form.bms_author.data
-        bga_author = form.bga_author.data
-        description = form.description.data
-        link = form.bms_link.data
-        database.insert_entry(name, author, bga_author, description, link)
-        return render_template("submit_success.html", name=name, author=author, link=link, success=True)
-    return render_template("submit_success.html", success=False)
+    if submissions_open:
+        form = SubmitForm()
+        if form.validate_on_submit():
+            name = form.bms_name.data
+            author = form.bms_author.data
+            bga_author = form.bga_author.data
+            description = form.description.data
+            link = form.bms_link.data
+            database.insert_entry(name, author, bga_author, description, link)
+            return render_template("submit_success.html", name=name, author=author, link=link, success=True)
+        return render_template("submit_success.html", success=False)
+    else:
+        return render_template("section_closed.html")
 
 @app.route('/admin')
 def evt_admin():
@@ -62,39 +72,49 @@ def evt_rules():
 
 @app.route("/impressions/")
 def evt_songs():
-    return render_template("impressions.html", entries=database.get_entries())
+    if impressions_open:
+        return render_template("impressions.html", entries=database.get_entries())
+    else:
+        return render_template("section_closed.html")
 
 @app.route("/impressions/id/<id>")
 def sng_impressions(id, form=None):
     if form is None:
         form = ImpressionForm()
-    impressions = database.get_impressions(id)
-    return render_template("songimpressions.html",
-        impressions=impressions,
-        song=database.get_song_by_id(id),
-        rating=database.get_song_rating(id),
-        form=form,
-        impression_count=len(impressions))
+
+    if impressions_open:
+        impressions = database.get_impressions(id)
+        return render_template("songimpressions.html",
+            impressions=impressions,
+            song=database.get_song_by_id(id),
+            rating=database.get_song_rating(id),
+            form=form,
+            impression_count=len(impressions))
+    else:
+        return render_template("section_closed.html")
 
 @app.route("/impressions/id/submit/<id>", methods=["POST"])
 def submit_impression(id):
-    form = ImpressionForm()
-    if form.validate_on_submit():
-        author = form.author.data
-        rating = form.rating.data
-        comment = form.comment.data
-        try:
-            rating = int(rating)
-            if rating < 0 or rating > 100:
-                raise ValueError()
-            ip = request.environ["REMOTE_ADDR"]
-            database.insert_impression(id, author, rating, comment, ip)
-        except ValueError:
-            flash("Rating out of bounds or not a number.")
+    if impressions_open:
+        form = ImpressionForm()
+        if form.validate_on_submit():
+            author = form.author.data
+            rating = form.rating.data
+            comment = form.comment.data
+            try:
+                rating = int(rating)
+                if rating < 0 or rating > 100:
+                    raise ValueError()
+                ip = request.environ["REMOTE_ADDR"]
+                database.insert_impression(id, author, rating, comment, ip)
+            except ValueError:
+                flash("Rating out of bounds or not a number.")
+        else:
+            flash("Incomplete form.")
+        return redirect("/impressions/id/{}".format(id))
     else:
-        flash("Incomplete form.")
-    return redirect("/impressions/id/{}".format(id))
+        return render_template("section_closed.html")
 
-app.debug = True
+
 if __name__ == '__main__':
     app.run()
